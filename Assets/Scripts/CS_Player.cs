fd018934233239757e4e3f3a08a5144ea5bf3730
Jesse Shiao <30820084+jesse850330@users.noreply.github.com>
2020-11-19 15:11:38 +1300

    New Script

---
+++ Assets/Scripts/CS_Player.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Player : MonoBehaviour
+{
+    enum slideVector { nullVector, up, down, left, right };
+    private Vector2 touchFirst = Vector2.zero;
+    private Vector2 touchSecond = Vector2.zero;
+    private slideVector currentVector = slideVector.nullVector;
+    private float timer;
+    public float offsetTime = 0.1f;
+    public float SlidingDistance = 80f;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    void OnGUI()
+    {
+        if (Event.current.type == EventType.MouseDown)
+
+        {
+            touchFirst = Event.current.mousePosition;
+        }
+        if (Event.current.type == EventType.MouseDrag)
+
+        {
+            touchSecond = Event.current.mousePosition;
+
+            timer += Time.deltaTime;
+
+            if (timer > offsetTime)
+            {
+                touchSecond = Event.current.mousePosition;
+                Vector2 slideDirection = touchFirst - touchSecond;
+                float x = slideDirection.x;
+                float y = slideDirection.y;
+
+                if (y + SlidingDistance < x && y > -x - SlidingDistance)
+                {
+                    if (currentVector == slideVector.left)
+                    {
+                        return;
+                    }
+                    Debug.Log("right");
+                    currentVector = slideVector.left;
+                }
+                else if (y > x + SlidingDistance && y < -x - SlidingDistance)
+                {
+                    if (currentVector == slideVector.right)
+                    {
+                        return;
+                    }
+
+                    Debug.Log("left");
+                    currentVector = slideVector.right;
+                }
+                else if (y > x + SlidingDistance && y - SlidingDistance > -x)
+                {
+                    if (currentVector == slideVector.up)
+                    {
+                        return;
+                    }
+
+                    Debug.Log("up");
+                    currentVector = slideVector.up;
+                }
+                else if (y + SlidingDistance < x && y < -x - SlidingDistance)
+                {
+                    if (currentVector == slideVector.down)
+                    {
+                        return;
+                    }
+
+                    Debug.Log("Down");
+                    currentVector = slideVector.down;
+                }
+
+                timer = 0;
+                touchFirst = touchSecond;
+            }
+            if (Event.current.type == EventType.MouseUp)
+            {
+                currentVector = slideVector.nullVector;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CS_Score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CS_Score : MonoBehaviour
+{
+    public static int Score;
+    public static int TotalScore;
+    public Text ShowScore;
+
+    void Start()
+    {
+        Score = 0;
+    }
+
+    void Update()
+    {
+        TotalScore = Score;
+        ShowScore.text = TotalScore.ToString();
+    }
+
+}
