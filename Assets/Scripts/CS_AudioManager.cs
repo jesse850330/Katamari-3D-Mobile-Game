fc9af19b9c6ea94ced6a007e1fa9828487132d5a
Jesse Shiao <30820084+jesse850330@users.noreply.github.com>
2020-11-19 15:21:06 +1300

    AudioManager

---
+++ Assets/Scripts/CS_AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class CS_AudioManager : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+ 
+    public void SetMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+ 
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("Background", volume);
+    }
+ 
+    public void SetSoundEffectVolume(float volume)
+    {
+        audioMixer.SetFloat("SoundEffect", volume);
+    }
+}
---
+++ Assets/Scripts/CS_BloomSetting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class CS_BloomSetting : MonoBehaviour
+{
+    PostProcessVolume m_Volume;
+    Vignette m_Vignette;
+    public float LowIntensity;
+    public float HighIntensity;
+
+    void Start()
+    {
+        m_Vignette = ScriptableObject.CreateInstance<Vignette>();
+        m_Vignette.enabled.Override(true);
+        m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_Vignette);
+        InvokeRepeating("ChangeIntensity", 1f, 1f);
+    }
+
+    void ChangeIntensity()
+    {
+        m_Vignette.intensity.Override(Random.Range(LowIntensity, HighIntensity));
+    }
+}
